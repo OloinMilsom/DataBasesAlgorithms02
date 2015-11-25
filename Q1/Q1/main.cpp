9d4daae2f0333ba99be1c46bcf24c6700c8db7f8
OloinMilsom <keporagebora@gmail.com>
2015-11-25 15:10:07 +0000

    first

---
+++ Q1/Q1/main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using namespace std;
+
+int main() {
+	vector<int> emptyVecInt;
+	vector<int> randVecInt;
+	for (int i = 0; i < 3; i++)
+	{
+		//randVecInt.push
+	}
+
+	system("PAUSE");
+}
